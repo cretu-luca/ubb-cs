c90c2db2b0bd1b8a01500fd502c29de0038fd643
kpunkk <lucac375@gmail.com>
2024-05-03 13:17:32 +0300

    :)

---
+++ semester2/graph-algorithms/C++/v2/lab4.cpp
@@ -0,0 +1,9 @@
+#pragma once
+#include "graph.h"
+
+/*  4. Write a program that, given a graph with costs, does the following:
+    - verify if the corresponding graph is a DAG and performs a topological sorting of the activities
+        using the algorithm based on predecessor counters;
+    - if it is a DAG, finds a highest cost path between two given vertices, in O(m+n).
+ */
+
