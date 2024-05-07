5a409e319952a0713d539894a144e510a41f657b
kpunkk <123098894+kpunkk@users.noreply.github.com>
2024-05-08 01:16:26 +0300

    Delete semester2/graph-algorithms/c++ directory

--- semester2/graph-algorithms/c++/v1/read_write.h
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include <fstream>
-#include "graph.h"
-using namespace std;
-
-Graph readFromFile(const string& filePath);
-void writeToFile(const string& filePath, const Graph& graph);
