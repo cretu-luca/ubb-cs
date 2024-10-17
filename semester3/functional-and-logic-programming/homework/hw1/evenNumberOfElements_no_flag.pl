4fbec05276d6b7c1729fc132c583787d391da0b9
Cretu Luca <123098894+cretuluca@users.noreply.github.com>
2024-10-17 23:17:03 +0300

    Update evenNumberOfElements_no_flag.pl

--- semester3/functional-and-logic-programming/homework/hw1/evenNumberOfElements_no_flag.pl
+++ semester3/functional-and-logic-programming/homework/hw1/evenNumberOfElements_no_flag.pl
@@ -1,8 +1,12 @@
 % at each iteration, we try to jump over two elements at once
 % left with one element -> odd number of elements
 % left with no elements -> even number of elements
+
+% evenNumberOfElements (List, Boolean)
+% (i, o) -> deterministic
+
 evenNumberOfElements([], true).
 evenNumberOfElements([_], false).
 
-evenNumberOfElements([_ | [_ | Rest]], Ans):-
+evenNumberOfElements([_ | [_ | Tail]], Ans):-
     evenNumberOfElements(Rest, Ans).
