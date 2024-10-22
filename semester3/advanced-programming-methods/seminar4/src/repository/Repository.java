22027724169ae61681cb2ec2d4499764887fe24f
cretuluca <lucac375@gmail.com>
2024-10-22 18:35:59 +0300

    first commit

--- semester3/advanced-programming-methods/seminar4/src/repository/Repository.java
+++ semester3/advanced-programming-methods/seminar4/src/repository/Repository.java
@@ -2,24 +2,8 @@ package repository;
 
 import model.ProgState;
 
-import java.util.ArrayList;
-import java.util.List;
+public interface IRepository {
+    ProgState getCurrentProgram();
 
-public class Repository implements IRepository{
-    private List<ProgState> repository;
-
-    public Repository() {
-        this.repository = new ArrayList<>();
-    }
-
-    @Override
-    public ProgState getCurrentProgram() {
-        return this.repository.getFirst();
-    }
-
-    @Override
-    public void add(ProgState s) {
-        this.repository.add(s);
-    }
-
+    void add(ProgState s);
 }
