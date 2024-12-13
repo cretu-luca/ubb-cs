31589b9d2009bab4541655069395fba44e17fe37
Cretu Luca <123098894+cretuluca@users.noreply.github.com>
2024-12-13 09:03:28 +0200

    Update lab7.sql

--- semester3/databases/lab7.sql
+++ semester3/databases/lab7.sql
@@ -1,171 +1,180 @@
-CREATE TABLE Student (
-    StudentID INTEGER PRIMARY KEY,
-    StudentName VARCHAR(50),
-    UniRankPosition INTEGER UNIQUE
-)
-
-SELECT * FROM Student
-DROP TABLE Student
-
-CREATE TABLE Supervisor (
-    SupervisorID INTEGER PRIMARY KEY,
-    YearsOfExperience INTEGER,
-    SupervisorName VARCHAR(50)
-)
-
-SELECT * FROM Supervisor
-DROP TABLE Supervisor
-
-CREATE TABLE Thesis (
-    ThesisID INTEGER PRIMARY KEY,
-    StudentID INTEGER,
-    SupervisorID INTEGER,
-    ThesisName VARCHAR(50),
-    FOREIGN KEY (StudentID) REFERENCES Student (StudentID) ON DELETE CASCADE,
-    FOREIGN KEY (SupervisorID) REFERENCES Supervisor (SupervisorID) ON DELETE CASCADE
-)
-
-SELECT * FROM Thesis
-DROP TABLE Thesis
-
-INSERT INTO Student (StudentID, UniRankPosition, StudentName) VALUES (1, 9, 'Luca'),
-                                                                     (2, 5, 'Cristi'),
-                                                                     (3, 1, 'Serban'),
-                                                                     (4, 2, 'David'),
-                                                                     (5, 4, 'Denis'),
-                                                                     (6, 3, 'Bogdan'),
-                                                                     (7, 6, 'Vasile')
-
-INSERT INTO Supervisor (SupervisorID, YearsOfExperience, SupervisorName) VALUES (1, 18, 'Onet-Marian Zsuzsanna'),
-                                                                                (2, 15, 'Zsigmond Imre'),
-                                                                                (3, 5, 'Nechita Mihai'),
-                                                                                (4, 24, 'Pop Horia')
-
-INSERT INTO Thesis (ThesisID, StudentID, SupervisorID, ThesisName) VALUES (2, 1, 3, 'PINNs for PDE Solutions'),
-                                                                          (1, 2, 3, 'Diffusion Models for Image Generation'),
-                                                                          (4, 3, 2, 'Procedural World Generation in Game Development'),
-                                                                          (3, 4, 1, 'Object Recognition in Computer Vision'),
-                                                                          (5, 5, 2, 'Containerized Virtual AI Environments')
-
-CREATE PROCEDURE GetIndexes
-    @TableName NVARCHAR(128)
-AS
 BEGIN
-    DECLARE @ObjectID INT;
-    SET @ObjectID = OBJECT_ID(@TableName);
+    DELETE FROM TestRunViews
+    DELETE FROM TestRunTables
+    DELETE FROM TestRuns
+    DELETE FROM TestViews
+    DELETE FROM TestTables
+    DELETE FROM Tests
+    DELETE FROM Tables
+    DELETE FROM Views
 
-    IF @ObjectID IS NULL
-    BEGIN
-        PRINT 'Table does not exist.';
-        RETURN;
-    END
+    INSERT INTO Tables (Name) VALUES
+        ('Budget'),
+        ('City'),
+        ('Department');
 
-    SELECT
-        i.name AS IndexName,
-        i.type_desc AS IndexType,
-        c.name AS ColumnName
-    FROM
-        sys.indexes i
-    JOIN
-        sys.index_columns ic ON i.object_id = ic.object_id AND i.index_id = ic.index_id
-    JOIN
-        sys.columns c ON ic.object_id = c.object_id AND ic.column_id = c.column_id
-    WHERE
-        i.object_id = @ObjectID
-    ORDER BY
-        i.type_desc, i.name, ic.key_ordinal;
+    INSERT INTO Views (Name) VALUES
+        ('CityPopulation'),
+        ('CityDepartments'),
+        ('CityDepartmentStats');
+
+    INSERT INTO Tests (Name) VALUES
+        ('delete_table_100'),
+        ('insert_table_100'),
+        ('select_view');
+
+    DECLARE @BudgetID INT = (SELECT TableID FROM Tables WHERE Name = 'Budget');
+    DECLARE @CityID INT = (SELECT TableID FROM Tables WHERE Name = 'City');
+    DECLARE @DeptID INT = (SELECT TableID FROM Tables WHERE Name = 'Department');
+    DECLARE @DeleteTestID INT = (SELECT TestID FROM Tests WHERE Name = 'delete_table_100');
+    DECLARE @InsertTestID INT = (SELECT TestID FROM Tests WHERE Name = 'insert_table_100');
+    DECLARE @SelectTestID INT = (SELECT TestID FROM Tests WHERE Name = 'select_view');
+
+    INSERT INTO TestTables (TestID, TableID, NoOfRows, Position) VALUES
+        (@DeleteTestID, @BudgetID, 100, 1),
+        (@DeleteTestID, @CityID, 100, 2),
+        (@DeleteTestID, @DeptID, 100, 3),
+        (@InsertTestID, @BudgetID, 100, 1),
+        (@InsertTestID, @CityID, 100, 2),
+        (@InsertTestID, @DeptID, 100, 3);
+
+    INSERT INTO TestViews (TestID, ViewID)
+    SELECT @SelectTestID, ViewID FROM Views;
 END;
-GO
 
-DROP PROCEDURE GetIndexes
-
-EXEC GetIndexes Student
-EXEC GetIndexes Supervisor
-EXEC GetIndexes Thesis
-
--- a)
-
--- CLUSTERED INDEX SCAN
-SELECT * FROM Student
-
--- CLUSTERED INDEX SEEK
-SELECT * FROM Student WHERE Student.StudentID = 1
-
--- NON CLUSTERED INDEX SCAN
-SELECT * FROM Student WHERE UniRankPosition > 2
-
--- NON CLUSTERED INDEX SEEK
-SELECT * FROM Student WHERE UniRankPosition = 2
-
--- KEY LOOKUP
-SELECT StudentName FROM Student WHERE UniRankPosition = 2
-
--- b)
-
--- without index
--- AvgRowSize = 44; EstimateCPU = 0.110157; EstimateIO = 0.470532; EstimateRebinds = 0; EstimateRewinds = 0; EstimateRows = 27939; EstimatedExecutionMode = Row; EstimatedRowsRead = 100000; EstimatedTotalSubtreeCost = 0.580689;
-
--- with index
--- AvgRowSize = 44; EstimateCPU = 0.0308899; EstimateIO = 0.0905324; EstimateRebinds = 0; EstimateRewinds = 0; EstimateRows = 27939; EstimatedExecutionMode = Row; EstimatedRowsRead = 27939; EstimatedTotalSubtreeCost = 0.121422;
-
-SELECT * FROM Supervisor WHERE YearsOfExperience > 15 and YearsOfExperience < 30
-
-CREATE PROCEDURE InsertSupervisorData
-    @RowCount INT
+CREATE OR ALTER PROCEDURE runTest
+    @TestName VARCHAR(50),
+    @TableName VARCHAR(50)
 AS
 BEGIN
     SET NOCOUNT ON;
+    DECLARE @TestID INT = (SELECT TestID FROM Tests WHERE Name = @TestName);
+    IF @TestID IS NULL RETURN;
 
-    DECLARE @counter INT = 1;
+    DECLARE @TestRunID INT;
+    DECLARE @StartTime DATETIME = GETDATE();
 
-    WHILE (@counter <= @RowCount)
+    INSERT INTO TestRuns (Description, StartAt, EndAt)
+    VALUES (@TestName, @StartTime, @StartTime);
+    SET @TestRunID = SCOPE_IDENTITY();
+
+    IF @TestName = 'delete_table_100'
     BEGIN
-        INSERT INTO Supervisor (SupervisorID, YearsOfExperience, SupervisorName)
-        VALUES (
-            @counter,
-            FLOOR(RAND() * 50),
-            'Supervisor ' + CAST(@counter AS VARCHAR(10))
-        );
+        DECLARE @DeleteStart DATETIME = GETDATE();
 
-        SET @counter = @counter + 1;
-    END;
+        IF @TableName = 'Department'
+            DELETE FROM Department;
+        ELSE IF @TableName = 'City'
+            DELETE FROM City;
+        ELSE IF @TableName = 'Budget'
+            DELETE FROM Budget;
+
+        INSERT INTO TestRunTables (TestRunID, TableID, StartAt, EndAt)
+        SELECT
+            @TestRunID,
+            TableID,
+            @DeleteStart,
+            GETDATE()
+        FROM TestTables
+        WHERE TestID = @TestID;
+    END
+
+    IF @TestName = 'insert_table_100'
+    BEGIN
+        DECLARE @InsertStart DATETIME = GETDATE();
+        DECLARE @RowCount INT;
+
+        SELECT @RowCount = NoOfRows
+        FROM TestTables tt
+        JOIN Tables t ON tt.TableID = t.TableID
+        WHERE t.Name = 'Budget' AND tt.TestID = @TestID;
+
+        IF @TableName = 'Budget'
+            EXEC InsertTestBudget @RowCount;
+        ELSE IF @TableName = 'City'
+            EXEC InsertTestCity @RowCount;
+        ELSE IF @TableName = 'Department'
+            EXEC InsertTestDepartment @RowCount;
+
+        INSERT INTO TestRunTables (TestRunID, TableID, StartAt, EndAt)
+        SELECT
+            @TestRunID,
+            TableID,
+            @InsertStart,
+            GETDATE()
+        FROM TestTables
+        WHERE TestID = @TestID;
+    END
+
+    IF @TestName = 'select_view'
+    BEGIN
+        DECLARE @ViewStart DATETIME = GETDATE();
+
+        INSERT INTO TestRunViews (TestRunID, ViewID, StartAt, EndAt)
+        SELECT
+            @TestRunID,
+            ViewID,
+            @ViewStart,
+            @ViewStart
+        FROM TestViews
+        WHERE TestID = @TestID;
+
+        IF @TableName = 'CityPopulation'
+            SELECT * FROM CityPopulation;
+        ELSE IF @TableName = 'CityDepartments'
+            SELECT * FROM CityDepartments;
+        ELSE IF @TableName = 'CityDepartmentStats'
+            SELECT * FROM CityDepartmentStats;
+    END
+
+    UPDATE TestRuns
+    SET EndAt = GETDATE()
+    WHERE TestRunID = @TestRunID;
 END;
 
-SELECT COUNT(SupervisorID) FROM Supervisor
+EXEC runTest 'delete_table_100', 'Department';
+EXEC runTest 'delete_table_100', 'City';
+EXEC runTest 'delete_table_100', 'Budget';
 
-EXEC InsertSupervisorData 100000
-SELECT * FROM Supervisor
+SELECT 'Budget' as TableName, COUNT(*) as Count FROM Budget
+UNION ALL
+SELECT 'City', COUNT(*) FROM City
+UNION ALL
+SELECT 'Department', COUNT(*) FROM Department;
 
-CREATE NONCLUSTERED INDEX YearsOfExperienceIndex
-ON Supervisor (YearsOfExperience)
-INCLUDE (SupervisorName);
+EXEC runTest 'insert_table_100', 'Budget';
+EXEC runTest 'insert_table_100', 'City';
+EXEC runTest 'insert_table_100', 'Department';
 
-DROP INDEX Supervisor.YearsOfExperienceIndex
+SELECT 'Budget' as TableName, COUNT(*) as Count FROM Budget
+UNION ALL
+SELECT 'City', COUNT(*) FROM City
+UNION ALL
+SELECT 'Department', COUNT(*) FROM Department;
 
--- c)
+EXEC runTest 'select_view', 'CityPopulation';
+EXEC runTest 'select_view', 'CityDepartments';
+EXEC runTest 'select_view', 'CityDepartmentStats';
 
-CREATE VIEW JoinView
-AS
+SELECT * FROM CityPopulation;
+SELECT * FROM CityDepartments;
+SELECT * FROM CityDepartmentStats;
+
 SELECT
-    st.StudentID,
-    st.StudentName,
-    su.SupervisorID,
-    su.SupervisorName,
-    su.YearsOfExperience,
-    t.ThesisName,
-    t.ThesisID
-FROM Student st
-INNER JOIN Thesis t ON st.StudentID = t.StudentID
-INNER JOIN Supervisor su ON t.SupervisorID = su.SupervisorID;
+    tr.Description as TestName,
+    tr.StartAt,
+    tr.EndAt,
+    DATEDIFF(millisecond, tr.StartAt, tr.EndAt) as Duration_ms
+FROM TestRuns tr
+ORDER BY tr.TestRunID;
 
-DROP VIEW JoinView
+SELECT * FROM Tests;
+SELECT * FROM Tables;
+SELECT * FROM Views;
 
-CREATE NONCLUSTERED INDEX StudentSupervisorIDSIndex
-ON Thesis (StudentID, SupervisorID)
-INCLUDE (ThesisName)
+SELECT * FROM TestTables;
+SELECT * FROM TestViews;
 
-DROP INDEX Thesis.StudentSupervisorIDSIndex
-
-SELECT * FROM JoinView
-
-SELECT * FROM JoinView WHERE YearsOfExperience > 15;
+SELECT * FROM TestRuns;
+SELECT * FROM TestRunTables;
+SELECT * FROM TestRunViews;
