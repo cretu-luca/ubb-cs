ecc6da56a41faa69cb810572f9b49393569c4c2d
Cretu Luca <123098894+cretuluca@users.noreply.github.com>
2025-01-08 23:22:17 +0200

    lisp?

---
+++ semester3/functional-and-logic-programming/homework/hw6/1.lisp
@@ -0,0 +1,19 @@
+; 1. Write a function to check if an atom is member of a list (the list is non-liniar)
+
+(defun mapcan-member (l e)
+  (cond
+    ((null l) nil)
+    ((atom l) (equal l e))
+    (t (if 
+        (mapcan #'(lambda (x) 
+                    (if (mapcan-member x e) 
+                        (list t)
+                        nil))
+                l)
+          t
+          nil)
+    )
+  )
+)
+
+(print (mapcan-member '(1 2 (4 (1) 6) 2 (4 (7 5)) 3 4 6) '5))
